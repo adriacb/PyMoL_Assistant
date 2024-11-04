7273b519d5155bff8cb09b1d93ed7d4c5e039677
Adrià Cabello Blanque <47384973+adriacb@users.noreply.github.com>
2024-11-04 16:20:42 +0100

    Actualizar prompts.py

--- pymol-assistant/src/prompts.py
+++ pymol-assistant/src/prompts.py
@@ -11,29 +11,9 @@ If there are no objects loaded, prioritize directing the user
 to load or fetch one before providing additional instructions.
 
 You have access to tools to search the PyMOL 
-documentation or to respond directly with an answer. 
+documentation. 
 Based on the query and any related documentation retrieved, 
 propose a suitable PyMOL function (always prefixed with "cmd.") 
 along with appropriate parameters, and briefly explain how to use 
 the function to address the user’s request.
 """
-
-RAG_LLM_PROMPT="""You are provided with a related query and a retrieved bunch of 
-documents from the PyMoL documentation. Your task is to propose a PyMoL function 
-and the appropriate parameters that can be used to address the query. 
-Provide a brief explanation of how the function and parameters can be used.
-
-Hint: the function is a python function, it starts always with "cmd." and the parameters are separated by commas.
-
-Context: {context}
-
-Please format your response using the following keys:
-- `resoning_steps`: reasoning behind the proposed function and parameters
-- `explanation`: a brief explanation of how the function and parameters can be used to address the query
-- `not_related`: a boolean value indicating if the query is not related to PyMoL
-- `missing_requirements`: an explanation of what is missing in case the command cannot be used
-- `usage`: string with the usage of the function and parameters.
-- `references`: a reference to the PyMoL documentation or any other source that supports your answer (if any)
-- `answer`: a friendly answer to the user's query providing the function and the references 
-"""
-# Please format your response as a json format (```json...```) with the following keys:
